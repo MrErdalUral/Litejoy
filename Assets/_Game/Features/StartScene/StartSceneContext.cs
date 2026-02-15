e3b5314ebad6a476ada7f500a1d35c5351db76a8
Erdal Ural <ahandaerdal@hotmail.com>
2026-02-15 13:27:31 +0100

    Created the empty scene context object
    It will main access point to the singleton references in the scene since I'm not including a DI for the purposes of this test

---
+++ Assets/_Game/Features/StartScene/StartSceneContext.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSceneContext : MonoBehaviour
+{
+    private static StartSceneContext _instance;
+
+    public static StartSceneContext Instance => _instance;
+
+    private void Awake()
+    {
+        _instance ??= this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
